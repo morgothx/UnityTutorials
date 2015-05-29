e0f779520745d14d86fd25de488277840137d259
Camilo <xnazgul@gmail.com>
2015-05-29 15:53:29 -0500

    8th Step:
    - Add Asteroids that can be destroy by the lasers

---
+++ Space Invaders/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour 
+{
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag != "Boundary") 
+		{
+			Destroy (other.gameObject);
+			Destroy (gameObject);
+		}
+	}
+}
