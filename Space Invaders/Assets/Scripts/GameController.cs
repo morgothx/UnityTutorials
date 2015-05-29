367f8dc358fd912544b7aaf32a6eb810557cbfed
Camilo <xnazgul@gmail.com>
2015-05-29 16:33:50 -0500

    10th Step:
    - Add Game Controller Script
    - Spawn 1 asteroid randomly

---
+++ Space Invaders/Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+	[SerializeField]
+	private GameObject hazard;
+
+	[SerializeField]
+	private Vector3 spawnValues;
+
+	void Start()
+	{
+		SpawnWaves ();
+	}
+
+	void SpawnWaves()
+	{
+		Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+		Quaternion spawnRotation = Quaternion.identity;
+		Instantiate (hazard, spawnPosition, spawnRotation);
+	}
+}
