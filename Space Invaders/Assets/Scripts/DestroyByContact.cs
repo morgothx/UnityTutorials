7d4ef7a76f548eff90d7540277130af08a2a083b
Camilo <xnazgul@gmail.com>
2015-05-29 16:08:10 -0500

    9th Step:
    - Add explosion when an asteroid is destroyed
    - Add explosion when the player crash with the asteriod
    - Add movement and rotation for the asteroid
    - Convert asteroid to prefab

--- Space Invaders/Assets/Scripts/DestroyByContact.cs
+++ Space Invaders/Assets/Scripts/DestroyByContact.cs
@@ -3,12 +3,26 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour 
 {
+	[SerializeField]
+	private GameObject explosion;
+
+	[SerializeField]
+	private GameObject playerExplosion;
+
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.tag != "Boundary") 
 		{
+			explode(other);
 			Destroy (other.gameObject);
 			Destroy (gameObject);
 		}
+	}
+
+	private void explode(Collider other)
+	{
+		Instantiate(explosion, transform.position, transform.rotation);
+		if(other.tag == "Player")
+			Instantiate(playerExplosion, transform.position, transform.rotation);
 	}
 }
